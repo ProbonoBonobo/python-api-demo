10722f7d02c2001d0804998f8898090fbbaff789
Nobody Knows I'm A Dog <kz@Nobodys-MacBook-Pro.local>
2017-05-08 18:48:46 -0700

    added REST demo

--- client/xmlrpc-client.py
+++ client/xmlrpc-client.py
@@ -1,6 +1,6 @@
 from xmlrpc.client import ServerProxy
 if __name__ == '__main__':
-  s = ServerProxy('http://localhost:15000', allow_none=True)
+  s = ServerProxy('http://gyrewinder.com:15000', allow_none=True)
   s.set('foo', 'bar')
   s.set('spam', [1, 2, 3])
   s.keys()
